ac909328fd153ca9fbee52505095c7d1a2969145
Chaitali Shirsath <chaitali3611@gmail.com>
2026-03-03 01:42:09 +0530

    Changing Menu Driven Code

--- Chapter-1-Searching-Sorting/Lab _Session/Menu-Driven-Code.c
+++ Chapter-1-Searching-Sorting/Lab _Session/Menu-Driven-Code.c
@@ -9,22 +9,22 @@ int main()
 {
     struct student s[10];
     
-    int i, pos, n;
+    int i, pos, n, a, choice;
 
-    printf("Enter the number of students: ");
+    printf("\nEnter the number of students: ");
     scanf("%d", &n);
     printf("\nEnter the student Information: ");
     for(i=0; i<n; i++)
     {
-        printf("Enter the %d Student name", i+1);
+        printf("\nEnter the %d Student name: ", i+1);
         scanf("%s", &s[i].name);
-        printf("Enter the %d Student roll number", i+1);
+        printf("\nEnter the %d Student roll number: ", i+1);
         scanf("%d", &s[i].rn);
     }
 
     do
     {
-        printf("----Menu----");
+        printf("\n----Menu----");
         printf("\n1.Insertion");
         printf("\n2.Deletion");
         printf("\n3.Updation");
@@ -33,7 +33,32 @@ int main()
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
 
-        printf("")
+        switch(choice)
+        {
+            case 1:
+            printf("\nIt's Insertion.");
+            printf("\nEnter position for insertion (0-9): ");
+            scanf("%d", &pos);
+
+            if(pos)
+            break;
+
+            case 2:
+            printf("\nIt's Deletion.");
+            break;
+
+            case 3:
+            printf("\nIt's Updation.");
+            break;
+
+            case 4:
+            printf("\nIt's Display.");
+            break;
+        }
+
+        printf("\nWhether you want to continue(1/0): ");
+        scanf("%d", &a);
     } while(a == 1);
+    return 0;
     
 }
